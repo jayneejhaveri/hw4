d68b0a8cb94282039e797fe353a0f788c6db9e73
jayneejhaveri <jaynee.jhaveri@kellogg.northwestern.edu>
2024-03-04 23:44:02 +0000

    hw4_progress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by({ "email" => params["email"] })
+    @user = User.find_by({ "username" => params["username"] })
     if @user != nil
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
@@ -16,8 +16,8 @@ class SessionsController < ApplicationController
     else
       flash["notice"] = "Nope."
       redirect_to "/login"
-    end
   end
+  
   end
 
   def destroy
