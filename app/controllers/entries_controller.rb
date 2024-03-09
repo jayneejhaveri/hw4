ece2ec3d55582db929d24d933324d771d6dc4893
jayneejhaveri <jaynee.jhaveri@kellogg.northwestern.edu>
2024-03-09 20:24:48 +0000

    hw4_image

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,10 +7,10 @@ class EntriesController < ApplicationController
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["uploaded_image"] = params["uploaded_image"]
+    @entry["occurred_on"] = params["occurred_on"]  
     @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = @user["id"]
+    @entry["user_id"] = @user["id"]    
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   def new
   end
-
+ 
+  def show
+    @user = User.find_by({ "id" => params["id"] })
+  end
+  
   def create
     if User.find_by({ "email" => params["email"] }) == nil
     @user = User.new
     @user["username"] = params["username"]
-    
     @user["email"] = params["email"]
     @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
